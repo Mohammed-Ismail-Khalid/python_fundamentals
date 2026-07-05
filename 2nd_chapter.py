c0f24d605138b27c291418440e0018da32596df3
Mohammed Ismail Khalid <iszabikhalid@gmail.com>
2026-07-05 12:19:29 +0530

    completed chapter two

--- 2nd_chapter.py
+++ 2nd_chapter.py
@@ -94,7 +94,78 @@ print(date[5:7]) # 09
 #print(date[8:]) # 20
 print(date[-2:]) # 20
 """
+"""
 #Data cleaning
+#white space cleanup
+white_space_example = " Engineering"
+print(white_space_example.lstrip()) # Engineering
+white_space_example = "Engineering "
+print(white_space_example.rstrip()) # Engineering
+white_space_example = "       Engineering "
+print(white_space_example.strip()) # Engineering
 
+#specify the character to remove
+example = "##AAA###"
+print(example.strip("#")) # AAA
 
+#next level
+text = "    Engineering"
+print(len(text)) 
+print(len(text.strip()))
 
+#to check how many white spaces removed
+no_of_whitespaces = len(text) - len(text.strip())
+is_my_data_clean = len(text) == len(text.strip())
+
+print("Is my data clean?:", is_my_data_clean)
+print("Number of white spaces to be removed:", no_of_whitespaces)
+
+#case conversion
+search = "Email ".lower().strip()
+data = " emAil".lower().strip()
+
+print(search == data)
+
+#Python Challenge
+#Turn the messsy sstring into a single clean summary with name, role, and age
+messy_string = "968-Maria, ( D@t@ Engineer );; 27y  "
+#output should be - name: maria | role: data engineer | age: 27
+
+name = messy_string[4:9].lower()
+role = messy_string[13:26].lower().strip("()").replace("@", "a")
+age = messy_string[-6:-3]
+
+print(f"name: {name} | role: {role} | age: {age}")
+
+#string seaching
+phone = "+48-176-12345"
+print(phone.startswith("+49"))
+
+email = "baraa@outlook.com"
+print(email.endswith("gmail.com"))
+
+print("@" in email)
+
+url = "https://api.company.com/v1/data"
+print("/api" in url)
+
+#.find method gives the index of the first occurrence of a substring in a string. If the substring is not found, it returns -1.
+
+phone1 = "+48-176-12345"
+phone2 = "48-654-16548"
+phone3 = "0048-654-16548"
+#print(phone1.find("-")) # 3
+print(phone1[phone1.find("-")+1:])
+print(phone2[phone2.find("-")+1:])
+print(phone3[phone3.find("-")+1:])
+
+#checking and validating string
+country = "USA"
+#country = "USA#"
+print(country.isalpha())
+
+phone = "4748409498490"
+#phone = "47-48409498490"
+#phone = "3.14" #here . is consdered a special character, so it will return False
+print(phone.isnumeric())
+"""
