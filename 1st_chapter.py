23e1f0f0c919225f875682eb321937908f99f41c
Mohammed Ismail Khalid <iszabikhalid@gmail.com>
2026-07-05 11:07:36 +0530

    modified properly

--- 1st_chapter.py
+++ 1st_chapter.py
@@ -23,69 +23,6 @@ Independent block of code that can be called multiple times with different input
 Methods are functions that are associated with objects and can be called on those objects. They are defined within a class and operate on the data contained within the object. Methods are used to perform actions or manipulate the state of an object.   
 Methods are functions belongs to a specific object or class and are called using the dot notation. They can access and modify the object's attributes and perform operations specific to that object.   
 '''
-text = 'hi'
-number = 10
-'''
-#print(type(text)) # <class 'str'>
-#print(type(number)) # <class 'int'>
-print(text.upper()) # HI
-print(number.bit_length()) # 4
-'''
 
-#Problem: Create 5 variables - each with a different data type
-'''
-1. Your age
-2. Your height (with decimals)
-3. Your name
-4. Are you a student? (True/False)
-5. Something with no value yet
-'''
-
-'''
-age = 32
-height = 5.4
-name = "Khalid"
-is_student = False
-to_be_determined = None
-
-print("Age is:", age, "My height is:", height, "My name is:", name, "Am I a student:", is_student, "Something with no value yet:", to_be_determined)
-'''
-
-#Adding for commit
-
-#text = """Python is easy to learn.
-#Python is powerful.
-#Many people love python"""
-
-#print(text)
-
-#Transformation
-#phone = "176-1234-56"
-#print(phone.replace("-", " "))
-
-#chain method
-#price = "$1,299.99"
-#print(price.replace("$", "").replace(",", "")) # 1299.99
-
-#Python challenge
-#Convert the messy phone number into a clean number format with only digits.
-
-"""
-number = "+49 (176) 123-4576"
-print(number.replace("+", "00").replace(" ", "").replace("(", "").replace(")", "").replace("-", ""))
-"""
-
-"""
-#f string - formatted string
-name = "Sam"
-age = 34
-is_student = False
-print(f"Person name is {name}, they are {age} years old, and student status is {is_student}.")
-"""
-"""
-#Transformation
-stamp = "2026-09-20 14:30"
-print(stamp.split(" "))
-"""
 
 #with this chapter one ends
