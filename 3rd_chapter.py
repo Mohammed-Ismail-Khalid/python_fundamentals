dced61a0071ae6fb9f709b941e140123f6321fed
Mohammed Ismail Khalid <iszabikhalid@gmail.com>
2026-07-07 18:19:18 +0530

    rounding numbers

--- 3rd_chapter.py
+++ 3rd_chapter.py
@@ -1,2 +1,36 @@
 #Python Numbers
 #Numeric has 3 types: int, float, complex
+"""
+x = 5
+y = 5.7
+z = 2 +3j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Math operators
+print(7 //2) #floor division means round number
+print(10 % 2) #modulus means remainder. Used for even or odd numbers
+
+x = 2
+#if we are adding x to itself then instead of writing x = x + 3 we can write x += 3
+x += 3 #notice we are adding math operator before equal sign. This is called augmented assignment operator
+print(x)
+x -= 1
+print(x)
+x *= 2
+print(x)
+"""
+#Rounding numbers
+#measure distance
+print(abs(2 - 10)) #absolute value
+
+
+
+
+
+
+
+
+
