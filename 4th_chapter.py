bde6af7b3af1afcf54fe08285eb576134c950498
Mohammed Ismail Khalid <iszabikhalid@gmail.com>
2026-07-08 22:11:32 +0530

    Control Flow

--- 4th_chapter.py
+++ 4th_chapter.py
@@ -1,4 +1,30 @@
 #Control Flow 
 #Zero in boolean is false
 #if we have some value in boolean then it shows True when print
-print(bool("A"))
+#print(bool("A"))
+"""
+
+#any and all functions
+email = ""
+phone = "017-1234"
+username = "baraa123"
+#Allows registration
+#if any field is filled
+print(any([email, phone, username]))
+
+#Allow registration
+#only of all fields is filled
+print(all([email, phone, username])) #all values should be true inorder to be true
+"""
+
+
+
+
+
+
+
+
+
+
+
+
