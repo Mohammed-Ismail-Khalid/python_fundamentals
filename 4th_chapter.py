6c94e022e1d0671349287054bb03f1899e3c6100
Mohammed Ismail Khalid <iszabikhalid@gmail.com>
2026-07-11 17:12:57 +0530

    Started

--- 4th_chapter.py
+++ 4th_chapter.py
@@ -16,7 +16,9 @@ print(any([email, phone, username]))
 #only of all fields is filled
 print(all([email, phone, username])) #all values should be true inorder to be true
 """
-
+#Comparisioin Operator
+print(10 != 10)
+#strings can be compared too, alphabetically. Also case sensitive
 
 
 
