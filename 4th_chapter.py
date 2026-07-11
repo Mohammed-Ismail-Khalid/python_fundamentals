dd17e0ed96312d15f24e64d8400ef9cd3a343ac3
Mohammed Ismail Khalid <iszabikhalid@gmail.com>
2026-07-11 18:21:50 +0530

    Done with 4th chapter, also 4 really good problems. Just 5th advanced problem is remaining

--- 4th_chapter.py
+++ 4th_chapter.py
@@ -43,7 +43,7 @@ email = True
 password = False
 print(email and password)
 
-#not operator
+#not operator(it changes the reality of the operator so to speak)
 print(not 3 > 2)
 print(not True)
 print(not not True)
@@ -64,12 +64,62 @@ is_banned = True
 
 #print(is_logged_in or is_guest and not is_banned) #when banned it should show false but the logic needs to be corrected here
 print((is_logged_in or is_guest) and not is_banned)
+
+#Membership (in) and (not in) operator
+#print("a" in "data")
+print("a" not in "data")
+print(3 not in [1,2,3])
+
+#Task
+#Security check: ensure the domain is not banned
+domain = "gmail.com"
+banned_domains = ["spam.com", "fake.org", "bot.net"]
+print(domain not in banned_domains)
+
+#Identity (is) operator
+#When variables are stored in memory, lets say a and b; here a will be assigned an ID(ex 30) and b(ex 40)
+#so in memory :"a" is variable name, 40 is its ID in memory and whatever value is in the variable "a"
+a = [1,2,3]
+b = [1,2,3]
+#to compare values, we use ==
+print(a == b) #shows True
+#to check wheather something is pointing to the same object, we check ID by using 'is' operator
+print(a is b) #is a same object as b
+
+#Example
+x = ["a", "b", "c"]
+y = x
+
+print(x == y)
+print(x is y)
+
+#Make sure the email exits, and it's not empty
+
+email = None
+#print(email != None and email != "")
+#here when dealing with 'None" best practise is to use "is" operator
+print(email is not None and email != "")
+
+#Python challenges
+#1) Check if a user's name is not empty and the age is greater than or equal to 18
+
+user_name = 'Khalid'
+age = 18
+print((not user_name == "") and age >= 18)
+
+#2) Check if the password is at least 8 characters long and does not contain space
+password = "12345 678"
+print (len(password) >= 8 and " " not in password)
+
+#3) Check if a users email is not empty, contains '@', and ends with '.com'
+user_email = 'iszabikhalid@gmail.com'
+#print(user_email[-4:])
+print(not user_email == '' and "@" in user_email and user_email[-4:] == ".com")
+
+#4) Check if a username is a string, is not None, and is longer than 5 character
+user_name = "Khalid"
+print(type(user_name) == str and user_name is not None and len(user_name) > 5)
 """
-
-
-
-
-
 
 
 
