85e51777fdc7c54f38f57d35126903ed4cce2da8
Mohammed Ismail Khalid <iszabikhalid@gmail.com>
2026-09-05 11:15:17 +0530

    Almost done with 6th Chap

--- 6th_chapter.py
+++ 6th_chapter.py
@@ -273,7 +273,12 @@ while answer != "yes":
     answer = input("Do you agree?(yes/no): ")
 print("Thank you")
 '''
-
+#to not have infinite loop
+while True:
+    answer = input("Do you agree? (yes/no): ")
+    if answer == "yes":
+        break
+print("Thank you")
 
 
 
